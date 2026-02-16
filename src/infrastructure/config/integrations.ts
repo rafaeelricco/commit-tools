c496554ed7db9adc75c9d257536aed8d250c6bb8
rafaeelricco <rafaelricco10@gmail.com>
2026-02-16 11:56:24 -0300

    Add file to configure OAuth integration dependencies
    - Introduce `integrations.ts` in configuration setup.
    - Define `Dependencies` type for external integration requirements, specifically OAuth credentials.
    - Implement `configureDependencies` to pull `clientId` and `clientSecret` from environment variables.

---
+++ src/infrastructure/config/integrations.ts
@@ -0,0 +1,19 @@
+export { type Dependencies, configureDependencies };
+
+import environment from "@infra/config/environment";
+
+type Dependencies = {
+  readonly oauth: {
+    readonly clientId: string;
+    readonly clientSecret: string;
+  };
+};
+
+function configureDependencies(): Dependencies {
+  return {
+    oauth: {
+      clientId: environment.GOOGLE_CLIENT_ID,
+      clientSecret: environment.GOOGLE_CLIENT_SECRET,
+    },
+  };
+}

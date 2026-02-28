be996d581de2b082623f8caa73cbbb04be4f08ce
rafaeelricco <rafaelricco10@gmail.com>
2026-02-28 17:22:15 -0300

    Clean up spacing and remove obsolete environment variable header comments

--- src/app/components/model-selector.tsx
+++ src/app/components/model-selector.tsx
@@ -3,6 +3,7 @@ export { ModelSelector, type Model, type ModelSelectorProps };
 import * as React from "react";
 
 import { Box, Text, useInput, useApp } from "ink";
+
 import chalk from "chalk";
 
 type Model = {
